8f998f5bcad1615c55ed087975625e23997fd095
shivhub09 <shivnagori2020@gmail.com>
2024-03-15 23:32:23 +0530

    shared preferences added for keeping user logged in

--- frontend/lib/features/Login/bloc/login_bloc.dart
+++ frontend/lib/features/Login/bloc/login_bloc.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
+import 'package:frontend/services/email.dart';
 import 'package:frontend/services/loginFunction.dart';
 import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
@@ -10,7 +11,6 @@ part 'login_state.dart';
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginInitial()) {
     on<LoginInitialEvent>(loginInitialEvent);
-
     on<LoginButtonClickedEvent>(loginButtonClickedEvent);
   }
 
@@ -30,6 +30,8 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     Map<String, dynamic> jsonResponse =
         await loginUser(event.email, event.password);
     if (jsonResponse["status"] == true) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('email', event.email);
       emit(LoginNavigateToHomeScreenActionState());
     }
     // print(response);
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/features/Login/ui/login.dart';
 import 'package:frontend/mainScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoggedInStatus();
+  }
+
+  Future<void> checkLoggedInStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? email = prefs.getString('email');
+
+    setState(() {
+      isLoggedIn = email != null && email.isNotEmpty;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPageStudent(),
+      home: isLoggedIn ? MainScreenPage() : LoginPageStudent(),
     );
   }
 }
